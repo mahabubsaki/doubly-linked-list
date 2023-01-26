6682f4c791a02ddea730429bcd5ae0486fed98bc
mahabubsaki <md929820@gmail.com>
2023-01-26 11:10:10 +0600

    structure done

--- doubly-linked-list.cpp
+++ doubly-linked-list.cpp
@@ -1,5 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
+class node
+{
+public:
+    int data;
+    node *next;
+    node *previous;
+};
+class DoublyLinkedList
+{
+    node *head;
+    int size;
+    DoublyLinkedList()
+    {
+        head = NULL;
+        size = 0;
+    }
+};
 int main()
 {
     int x = 3;
